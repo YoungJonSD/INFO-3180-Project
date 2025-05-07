c0f23d083c58192f22640c9a7c44380ee8015877
Matthew Cross <matthewkacross@gmail.com>
2025-05-07 14:31:54 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Use relative paths
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
